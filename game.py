c2cfc359c35d572fc060f29a707d2129d6da4701
angie <aberroya@ucvts.org>
2019-01-15 15:36:34 +0000

    commit4

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import random
 
+numwins = 0
+numlosses = 0
+
 class Fighter:
     def __init__(self, name, hp, atk, defense, accuracy, attacks, weapon):
         self.name = name
@@ -23,24 +26,7 @@ class Fighter:
         elif choice == 'Wizard':
             print (f" The special stat for this Fighter is mana. Mana: Unfortunately {self.mana}")
 
-    def fight (self):
-        assailants = [Paladin, Berserker, Wizard]
-        opp = random.choice(assailants)
-        print ("you have encountered",opp,"!")
-        FightChoice = input("Will you proceed to battle?")
-        if FightChoice == ('yes'):
-            print ("You have encountered",opp,"! Good Luck. You Will Need Copious Amounts Of It.")
-            opp.hp -= player.atk
-            showoppstatus()
-            playerhealth -= opp.atk
-            winorlose()
-
-class Wizard(Fighter):
-    def __init__(self, name, hp, atk, defense, accuracy, attacks, weapon, mana):
-        Fighter.__init__(self, name, hp, atk, defense, accuracy, attacks, weapon)
-        self.mana = mana
-
-    def showoppstatus (self):
+    def showoppstatus(self):
         """display stats of opponent"""
         print (f"{self.name}:")
         print(f"- HP: {self.hp}")
@@ -49,6 +35,29 @@ class Wizard(Fighter):
         print (f" - Accuracy: {self.accuracy}")
         print (f" - Attacks: {self.attacks}")
         print (f" - Weapon: {self.weapon}")
+
+    def fight (self,opp):
+        assailants = [Paladin, Berserker, Wizard]
+        opp = random.choice(opponents)
+        print ("you have encountered",opp,"!")
+        FightChoice = input("Will you proceed to battle?")
+        if FightChoice == ('yes'):
+            for n in range (3):
+                print ("You have encountered",opp,"! Good Luck. You Will Need Copious Amounts Of It.")
+                opp.hp -= Player.atk
+                opp.showoppstatus()
+                Player.hp -= opp.atk
+                winorlose(self)
+
+class Player(Fighter):
+    def __init__(self, name, hp, atk, defense, accuracy, attacks, weapon):
+        Fighter.__init__(self, name, hp, atk, defense, accuracy, attacks, weapon)
+
+
+class Wizard(Fighter):
+    def __init__(self, name, hp, atk, defense, accuracy, attacks, weapon, mana):
+        Fighter.__init__(self, name, hp, atk, defense, accuracy, attacks, weapon)
+        self.mana = mana
 
 
 class Berserker(Fighter):
@@ -82,51 +91,37 @@ class Paladin(Fighter):
         print (f" - Attacks: {self.attacks}")
         print (f" - Weapon: {self.weapon}")
 
+Player = Player('the girl reading this uwu',350,50,50,100,['totally normal punch','student loans','osmosis'],'left toe')
+Berserker = Berserker('Physics Himself',5000,500,50,50,['test fail','centripetal force','free body kick'],'mega spring',100)
+Paladin = Paladin('Waluigi',700,100,25,75,['WAH','Luigi Disguise','M E G A  W A H'],'pointy tennis racket',80)
+Wizard = Wizard('Danny DeVito',250,300,75,25,['Confusion','Astonishment','Wonder'],'the scepter of truth',75)
 
-Berserker = Berserker('Physics Himself','5000','500','50','50',['test fail','centripetal force','free body kick'],'mega spring','100')
-Paladin = Paladin('Waluigi','700','100','25','75',['WAH','Luigi Disguise','M E G A  W A H'],'pointy tennis racket','80')
-Wizard = Wizard('Danny DeVito','250','300','75','25',['Confusion','Astonishment','Wonder'],'the scepter of truth','75')
-
-winmessages: ['You made it this far? Not for long. Proceed.', 'Turn your hacks off we know youre cheating if you survived that','VICTORY ROYALE! NEXT BATTLE, GAMER.']
-
-
+winmessages = ['You made it this far? Not for long. Proceed.', 'Turn your hacks off we know youre cheating if you survived that','VICTORY ROYALE! NEXT BATTLE, GAMER.']
 
 
 def showstatus(self):
-    print (f"Your hp is at a low, low {playerhealth}")
+    print (f"Your hp is at a low, low {Player.hp}")
 
 def winorlose(self):
-    if playerhealth <= 0:
+    if Player.hp <= 0:
         print ("You unfortunately did not make enough power moves to survive. GAME OVER :(")
-    elif playerhealth > 0:
+    elif Player.hp > 0:
         print (random.choice(winmessages))
-        showstatus()
-        fight()
+        showstatus(self)
+        opp.fight(opp)
 
-def fight (self):
-    assailants = [phys, wah, dd]
-    opp = random.choice(assailants)
-    print ("you have encountered",opp,"!")
-    print()
-    FightChoice = input("Will you proceed to battle?")
-    print()
-    if FightChoice == yes:
-        print ("You have encountered",opp,"! Good Luck. You Will Need Copious Amounts Of It.")
-        opp.hp -= player.atk
-        showoppstatus()
-        playerhealth -= opp.atk
-        winorlose()
+
 
 opponents = [Berserker, Paladin, Wizard]
 
 
-print ("Welcome Young One, To The Battle Royale Of The Century.")
+print ("Welcome Young One, To The Battle Royale Of The Century. If you survive three trials with the strongest of fighters, you have the chance to duel with the Ultimate Monster.")
 print ()
 playername = input("What Is The Name That Has Been Bestowed Upon You?")
 print ()
-playerhealth = 500
 print (f"{playername}, I Hope You Are Prepared For Battle.")
 print()
+
 choice = input("You have the choice to battle a Berserker, Paladin, or Wizard. Which stats would you like to see?")
 if choice == ('Paladin'):
     Paladin.showoppstatus()
@@ -136,12 +131,7 @@ elif choice == ('Wizard'):
     Wizard.showoppstatus()
 
 opp = random.choice(opponents)
-if opp == Paladin:
-    Paladin.showoppstatus()
-elif opp == Berserker:
-    Berserker.showoppstatus()
-elif choice == Wizard:
-    Wizard.showoppstatus()
-opp.fight()
+
+opp.fight(opp)
 
 
